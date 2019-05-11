12f514f38c2d0ffba49ad93419e38087e3ae96e4
AMCA15 <38790021+AMCA15@users.noreply.github.com>
2019-05-10 22:40:48 -0400

    Change script to a class implementation

--- GUI/fir.m
+++ GUI/fir.m
@@ -1,48 +1,89 @@
-n = 20;
-f = 0.5;
-
-f1 = 0.3;
-f2 = 0.6;
-
-m = 127;
-
-% FilterType
-%   1: LowPass     3: BandPass
-%   2: HighPass    4: BandStop
-FilterType = 2;
-
-switch FilterType
-    case {1, 2}
-        f = [0 f f 1];
-        if FilterType == 1
-            m = [m m 0 0];   
-        elseif FilterType == 2
-            m = [0 0 m m];   
+classdef fir < handle
+    properties
+        % Filter parameters
+        n
+        f
+        f1
+        f2
+        m
+        FilterType
+    end
+    
+    properties(Dependent)
+        coeff = round(fir2(obj.n, obj.f_, obj.m_));
+    end
+    
+    properties(Hidden)
+        freq
+        mag
+    end
+    
+    properties(Dependent, Hidden)
+        coeff_format
+    end
+        
+    methods
+        function obj = fir()
+            obj.n = 20;
+            obj.f = 0.5;
+            obj.f1 = 0.3;
+            obj.f2 = 0.6;
+            obj.m = 127; 
+            obj.FilterType = 'lowpass';
         end
-    case {3, 4}
-        f = [0 f1 f1 f2 f2 1];
-        if FilterType == 3
-            m = [0 0 m m 0 0];   
-        elseif FilterType == 4
-            m = [m m 0 0 m m];   
+        function value = get.coeff(obj)
+            switch obj.FilterType
+                case 'lowpass'
+                    obj.freq = [0 obj.f obj.f 1];
+                    obj.mag = [obj.m obj.m 0 0];   
+                case 'highpass'
+                    obj.freq = [0 obj.f obj.f 1];
+                    obj.mag = [0 0 obj.m obj.m];   
+                case 'bandpass'
+                    obj.freq = [0 obj.f1 obj.f1 obj.f2 obj.f2 1];
+                    obj.mag = [0 0 obj.m obj.m 0 0];                       
+                case 'bandstop'
+                    obj.freq = [0 obj.f1 obj.f1 obj.f2 obj.f2 1];
+                    obj.mag = [obj.m obj.m 0 0 obj.m obj.m];                       
+            end
+            value = round(fir2(obj.n, obj.freq, obj.mag));
         end
+        
+        function coeff_format = get.coeff_format(obj)
+            str = sprintf('%d, ', obj.coeff);
+            coeff_format = sprintf("{%s}\n", str(1:end-2));
+        end
+                
+        function plot(obj)
+            [h1, w] = freqz(obj.coeff, 1);
+            plot(obj.freq, obj.mag/obj.m, w/pi, abs(h1)/obj.m);
+            ylabel('Normalized Amplitude');
+            xlabel('Normalized Frecuency (F)');
+            legend('Ideal', sprintf("n = %d", obj.n));
+            grid on;
+        end
+        
+        function dispCoeff(obj)
+            fprintf(obj.coeff_format);
+        end
+        
+        function copyCoeff(obj)
+            clipboard('copy', obj.coeff_format); 
+        end
+        
+        function value = coeffPacked(obj)
+            value = int8([20 obj.coeff]);
+        end
+        
+    end
+    
+    methods(Static)
+        function value = enableBuzzer
+            value = int8(17);
+        end
+        function value = disableBuzzer
+           value = int8(16); 
+        end 
+    end
+    
 end
-
-
-bi = round(fir2(n, f, m));
-
-[h1, w] = freqz(bi, 1);
-
-plot(f, m/max(m), w/pi, abs(h1)/max(m));
-ylabel('Normalized Amplitude');
-xlabel('Normalized Frecuency (F)');
-legend('Ideal', sprintf("n = %d", n));
-grid on;
-
-str = sprintf('%d, ', bi);
-coeff_format = sprintf("normal {%s}\n", str(1:end-2));
-fprintf(coeff_format);
-
-clipboard('copy', coeff_format);
-
-clear;
